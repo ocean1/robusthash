d1a3ae8be334d9d68375fe22cb2a6b48b0638a8a
ocean1 <ocean.kuzuri@gmail.com>
2015-06-30 17:46:06 +0200

    use gray colormap

--- softhash.py
+++ softhash.py
@@ -5,6 +5,8 @@ import numpy
 from matplotlib import pyplot, cm
 import random
 
+import logging
+
 import matplotlib
 matplotlib.interactive(True)
 matplotlib.use("TkAgg")
@@ -12,12 +14,54 @@ matplotlib.use("TkAgg")
 
 DEBUG = True
 
+logger = logging.getLogger("softhash")
+ch = logging.StreamHandler()
+if DEBUG:
+    logger.setLevel("DEBUG")
+    ch.setLevel("DEBUG")
+else:
+    logger.setLevel("WARNING")
+    ch.setLevel("WARNING")
+
+logger.addHandler(ch)
+
+
+def _getblock(M, block, blocksize):
+    """ return the selected block of the given matrix
+    """
+    start = numpy.array(block) * blocksize
+
+    B = M[start[0]:start[0] + blocksize,
+          start[1]:start[1] + blocksize]
+
+    logger.warning("index = %s", start)
+    logger.debug("selected block: %s", B)
+    return B
+
 
 class SoftHash(object):
 
+    """
+        this class contains the implementation for the softhash,
+        it works by initializing it giving a key and a blocksize,
+        then one can always modify the "img" property and use the
+        update method to get the new hash
+        this way this can be used as a module and filters and other
+        operations on the image can be added without modifying it
+    """
+
     _block_size = 8     # use an 8x8 blocksize for the DCT
     _img = None    # the original image we are working on
-    _img_cropped = None  # the cropped image since we work on 8x8 blocks
+    _hash = None    # contains the current hash
+
+    @property
+    def img(self):
+        return self._img
+
+    @img.setter
+    def img(self, value):
+        self._img = value
+
     _color_image = False  # tells if the image is grayscale or color
 
     """ _key is a parameter which tells us how many blocks we select
@@ -32,38 +76,42 @@ class SoftHash(object):
     def __init__(self, imagefile, key, blocksize=8):
 
         self._block_size = blocksize
-        self._img = cv2.imread(imagefile, cv2.CV_LOAD_IMAGE_UNCHANGED)
+        self.img = cv2.imread(imagefile, cv2.CV_LOAD_IMAGE_UNCHANGED)
         # cv2 loads images in BGR. BGR -> RGB conversion needed
 
-        if self._img.shape[2] == 3:
+        if self.img.shape[2] == 3:
             self._color_image = True
-            # it's a color image let's convert! :)
-            self._img = cv2.cvtColor(self._img, cv2.COLOR_BGR2RGB)
+            # it's a color image let's convert!
+            # this way we can show it using pyplot
 
-        self._blocks = (numpy.array(self._img.shape[:2]) /
+            self.img = cv2.cvtColor(self.img, cv2.COLOR_BGR2RGB)
+
+        self._blocks = (numpy.array(self.img.shape[:2]) /
                         self._block_size)
         h, w = self._blocks * self._block_size
         # since we are using 256x256 pixel images this step
         # shouldn't be needed but it is here just for completeness
-        print self._blocks
-        print h, w
+        logger.debug("cropped image size")
+        logger.debug("h,w: %s", (h, w))
+        logger.debug("blocks: %s", self._blocks)
 
-        self._img_cropped = self._img[:h, :w]
+        self.img = self.img[:h, :w]
 
         self.initializeKey(key)
 
     def initializeKey(self, key):
         random.seed(key)  # initialize RNG
 
-        blocks = self._blocks[0] * self._blocks[1]
-        # get the total number of blocks and use it to get the random blocks
-        self._key = random.sample(range(0, blocks), self._keysize)
-        if DEBUG:
-            print "Key is: %s" % self._key
+        # create the key selecting which blocks we are going to use
+        self._key = zip(
+            random.sample(range(0, self._blocks[0]), self._keysize),
+            random.sample(range(0, self._blocks[1]), self._keysize))
+
+        logger.debug("Key is: %s", self._key)
 
     def plotImg(self, name=None):
 
-        pyplot.imshow(self._img)
+        pyplot.imshow(self.img)
 
     def denoise(self):
         if self.is_color:
@@ -77,14 +125,79 @@ class SoftHash(object):
         """
         apply the given filter to the image
         """
-        imgfilter(self._img, self._img)
+        imgfilter(self.img, self.img)
         pass
 
-    def hash(self):
+    def update(self):
         # by default we denoise the image using non-local means algorithm
+        # should denoising be done before or after resizing?
         self.denoise()
 
-        # step 2 we can
+        # we will use the luminance channel
+        # if it's greyscale just use the existing channel
+        if self.is_color:
+            _img = cv2.cvtColor(self.img, cv2.COLOR_RGB2YCR_CB)
+            Y = numpy.zeros((_img.shape[0], _img.shape[1]), dtype=_img.dtype)
+            Y[:, :] = _img[:, :, 0]
+
+            # Cr = numpy.zeros((_img.shape[0], _img.shape[1]), dtype=_img.dtype)
+            # Cr[:, :] = _img[:, :, 1]
+            # Cb = numpy.zeros((_img.shape[0], _img.shape[1]), dtype=_img.dtype)
+            # Cb[:, :] = _img[:, :, 2]
+
+        else:
+            Y = self.img
+
+        logger.debug("Y: %s", Y)
+        pyplot.figure('Y channel')
+        pyplot.imshow(Y, cmap=pyplot.get_cmap('gray'))
+        # pyplot.figure('Cr channel')
+        # pyplot.imshow(Cr)
+        # pyplot.figure('Cb channel')
+        # pyplot.imshow(Cb)
+
+        # now we use the selected block with the given key
+
+        logger.debug("using blocks of size %d", self._block_size)
+
+        for block in self._key:
+            B = _getblock(Y, block, self._block_size)
+
+    """        TransAll = []
+            TransAllQuant = []
+            ch = ['Y', 'Cr', 'Cb']
+            plt.figure()
+            for idx, channel in enumerate(imSub):
+                plt.subplot(1, 3, idx + 1)
+                channelrows = channel.shape[0]
+                channelcols = channel.shape[1]
+                Trans = np.zeros((channelrows, channelcols), np.float32)
+                TransQuant = np.zeros((channelrows, channelcols), np.float32)
+                blocksV = channelrows / B
+                blocksH = channelcols / B
+                vis0 = np.zeros((channelrows, channelcols), np.float32)
+                vis0[:channelrows, :channelcols] = channel
+                vis0 = vis0 - 128
+                for row in range(blocksV):
+                    for col in range(blocksH):
+                        currentblock = cv2.dct(
+                            vis0[row * B:(row + 1) * B, col * B:(col + 1) * B])
+                        Trans[
+                            row * B:(row + 1) * B, col * B:(col + 1) * B] = currentblock
+                        TransQuant[
+                            row * B:(row + 1) * B, col * B:(col + 1) * B] = np.round(currentblock / Q[idx])
+                TransAll.append(Trans)
+                TransAllQuant.append(TransQuant)
+                if idx == 0:
+                    selectedTrans = Trans[
+                        srow * B:(srow + 1) * B, scol * B:(scol + 1) * B]
+                else:
+                    sr = np.floor(srow / SSV)
+                    sc = np.floor(scol / SSV)
+                    selectedTrans = Trans[sr * B:(sr + 1) * B, sc * B:(sc + 1) * B]
+                plt.imshow(selectedTrans, cmap=cm.jet, interpolation='nearest')
+                plt.colorbar(shrink=0.5)
+                plt.title('DCT of ' + ch[idx])"""
 
     @property
     def is_color(self):
@@ -92,32 +205,25 @@ class SoftHash(object):
 
 
 if __name__ == "__main__":
-    sf = SoftHash('./ImageDatabaseCrops/NikonD60/DS-01-UTFI-0000-0_crop.TIF')
-    # sf = SoftHash('Ub7XL8T.png', 1234)
+    # sf = SoftHash(
+    #    './ImageDatabaseCrops/NikonD60/DS-01-UTFI-0196-0_crop.TIF',
+    #    1234)
+    sf = SoftHash('test.png', 1234)
 
-    if DEBUG:
-        pyplot.figure('original')
-        sf.plotImg()
+    sf.update()
 
-    sf.denoise()  # let's denoise it!
-
-    # ok now let's
-
-    if DEBUG:
-        pyplot.figure('denoised')
-        sf.plotImg()
+    # TODO: based on the keysize we can decide to resize the image
+    # to match the final key size :)
 
     pyplot.show(block=True)
 
 
 """
-looks like: 'The spatial filtering applied by the human visual system appears
+'The spatial filtering applied by the human visual system appears
 to be low pass for chromatic stimuli and band pass for luminance stimuli'
-http://www.ncbi.nlm.nih.gov/pubmed/9499586 so we decided to use the
+http://www.ncbi.nlm.nih.gov/pubmed/9499586
 
-so we are going to use the luminance and a RGB byte which is the DC component
-of every channel to define the dominant color in the 8x8 block, this could help
-discriminating images which have a similar luminance
+so we are going to use the luminance
 
 hash structure:
 - for each 8x8 block composing the picture we select the low frequencies
